df446e6682bcf025427fab6417f2e2fb76e55ff7
rubrixks <rgarcia4056@gmail.com>
2021-12-18 17:52:06 -0600

    added withdraw function, beginGame function, light testing

--- contracts/TheGame.sol
+++ contracts/TheGame.sol
@@ -15,11 +15,11 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 /// @custom:experimental This is an experimental contract.
 contract TheGame is ERC721,ERC721URIStorage, Ownable {
 
-
+  bool public GameStarts = false;
   uint public totalSupply = 2**256-1;
   uint public totalLosses =0;
   string public baseURI = "https://bafkreigm33tvwbd3t6led7tltb5ff67zvbhofihj7axifto5f7ffnsmghm.ipfs.dweb.link/";
-  string public AccountwithMostLs;
+  uint public LossBasePrice = .00025 ether;
   mapping (address=>uint) LossTracker;
   using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
@@ -31,57 +31,67 @@ contract TheGame is ERC721,ERC721URIStorage, Ownable {
 
     /// @notice Allows creator to mint the first 100 losses
     /// @dev Only the owner can call this function
-    function FirstLoser() public onlyOwner  {
-        for (uint i=0; i<3;i++) {
+    function FirstLoser() payable public onlyOwner  {
+      beginTheGame();
+      require(msg.sender == owner(), "Only the owner can call this function.");
+      require(msg.value == LossBasePrice * 10,"Not enough Ether was sent");
+        for (uint i = 0; i < 10; i++) {
           safeMint(msg.sender);
-          totalLosses = totalLosses + 1;
-          LossTracker[msg.sender] = LossTracker[msg.sender] + amountOfLosses;
         }
-
-          
+        totalLosses = totalLosses + 10;
+        LossTracker[msg.sender] = LossTracker[msg.sender] + 10;
     } 
     ///Needs to HandOutLs after owner is given first 100 Ls;
-    /// @notice Anyone who has thought about the game can mint as many Losses as they have thought of 
-    /// @dev Keeps track of how many Losses have been minted 
-    /// @param address wanting to mint and amount of losses wanted on the address
-    function HandOutLs(address to, uint amountOfLosses) public {
-      require(amountOfLosses<= 100000,"No. Stop. Stop thinking about The Game.")
-        for (uint i = 0;i<amountOfLosses;i++) {
+    /// notice Anyone who has thought about the game can mint as many Losses as they have thought of 
+    /// dev Keeps track of how many Losses have been minted 
+    /// param address wanting to mint and amount of losses wanted on the address
+    function HandOutLs(address to, uint amountOfLosses) payable public {
+      require(GameStarts == true,"The Game has not begun");
+      require(amountOfLosses <= 100000,"Stop thinking about The Game.");
+      require(msg.value == LossBasePrice * amountOfLosses,"Sorry friend,  send the exact amount of Ether required");
+        for (uint i = 0; i < amountOfLosses; i++) {
           safeMint(to);
         }
         totalLosses = totalLosses + amountOfLosses;
         LossTracker[msg.sender] = LossTracker[msg.sender] + amountOfLosses;
         }
-  /// @notice Calculate tree age in years, rounded up, for live trees
-    /// @dev safely mints NFT
-    /// @param to is the address of the address wanting to mint NFT
+  /// notice Calculate tree age in years, rounded up, for live trees
+    /// dev safely mints NFT
+    /// param to is the address of the address wanting to mint NFT
     function safeMint(address to) internal {
         uint tokenId = _tokenIdCounter.current()+1;
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, baseURI);
     }
+
     // The following functions are overrides required by Solidity.
     ///NEEDS TO BE FIXED
-    /// @notice burns the Loss NFT from the address requesting to burn
-    /// @dev Only the owner can burn their Loss
-    /// @param tokenID so the address can identify which loss to get rid of the NFT
+    /// notice burns the Loss NFT from the address requesting to burn
+    /// dev Only the owner can burn their Loss
+    /// param tokenID so the address can identify which loss to get rid of the NFT
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
     }
-    /// @notice Returns JSON file of the The Game
-    /// @dev Must return baseURI for all tokenURI requests
-    /// @param tokenId is what makes the token nonfungible, it keeps track of when the NFT want minted
-    /// @return URI
+    /// notice Returns JSON file of the The Game
+    /// dev Must return baseURI for all tokenURI requests
+    /// param tokenId is what makes the token nonfungible, it keeps track of when the NFT want minted
+    /// return URI
     function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
-    /// @notice Returns who the amount of Losses on in one address
-    /// @dev Must return baseURI for all tokenURI requests
-    /// @param ad is the address that tracks how many times the account has minted or has been transferred losses
-    /// @return a number
-    function LossFinder(address ad) public view returns(uint) {
+    /// notice Returns who the amount of Losses on in one address
+    /// dev Must return baseURI for all tokenURI requests
+    /// param ad is the address that tracks how many times the account has minted or has been transferred losses
+    /// returns a number
+    function lossFinder(address ad) public view returns(uint) {
       return LossTracker[ad];
     }
-
+    function withdraw() payable public onlyOwner {
+        uint balance = address(this).balance;
+        payable(msg.sender).transfer(balance);
+    }
+    function beginTheGame() public onlyOwner {
+        GameStarts = !GameStarts;
+    }
 }
